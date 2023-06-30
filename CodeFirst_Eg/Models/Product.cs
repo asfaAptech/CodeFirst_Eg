c5cf61fa781247b9e69bec7c92827ada8764d3af
asfaAptech <asfa.shaikh@gmail.com>
2023-06-30 18:21:41 +0530

    Add project files.

---
+++ CodeFirst_Eg/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace CodeFirst_Eg.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int UnitsInStock { get; set; }
+
+    }
+}
